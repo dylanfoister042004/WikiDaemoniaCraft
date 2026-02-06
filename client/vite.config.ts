7559fac3afab2c05806a72b37d9dc553ac06da15
Ultisyn <dylanfoister042004@gmail.com>
2026-02-06 17:28:30 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,1 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173
+  },
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        }
+      }
+    }
+  }
+});
 
