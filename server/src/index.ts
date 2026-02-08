07098c525fc0489c440c8176f60b0a03bfa61ab0
Ultisyn <dylanfoister042004@gmail.com>
2026-02-08 15:04:50 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import wikiRoutes from "./routes/wiki";
 import { logger } from "./utils/logger";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -18,13 +19,18 @@ app.use((req, _res, next) => {
 
 // Routes
 app.use("/api/wiki", wikiRoutes);
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Default route
 app.get("/", (_req, res) => {
   res.json({ status: "Daemonia Backend OK", version: "1.0.0" });
+});
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // Start server
 app.listen(PORT, () => {
   logger(`Daemonia Backend running on port ${PORT}`);
 });
+
